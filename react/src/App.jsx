daf96cd7291be76fcd9baf407859425ac112fd45
NEFEDOV_IT <eliaz_one@mail.ru>
2022-03-30 16:59:07 +0300

    start project

---
+++ react/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+
+      <div className="wrapper">
+        <header className="header">
+          Resource Company LTD.
+        </header>
+        <main className="main">
+          <div className="container">
+            <div className={'app'}>
+
+            </div>
+          </div>
+        </main>
+        <footer className="footer">Powered by NEFEDOV_IT and Simeon_Ermolin</footer>
+      </div>
+  )
+}
+
+export default App
