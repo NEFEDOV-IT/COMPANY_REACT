19b6fe4389456f39ea00b8dd5608d0074d5a82a4
SimeonErmolin <ermolinsemen96@gmail.com>
2022-04-04 21:40:26 +0300

    add file

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -9,6 +9,7 @@ import { Header } from "./Components/Header";
 import { WorkTime } from "./Components/Worktime";
 import { Footer } from "./Components/Footer";
 import { GraphTon } from "./Components/GraphTon";
+import GoldView from './Components/GoldView';
 
 removeHash()
 configureAnchors({offset: -60, scrollDuration: 800})
@@ -16,15 +17,16 @@ configureAnchors({offset: -60, scrollDuration: 800})
 function App() {
     return (
         <div className="wrapper">
-            <Header/>
-            <WorkTime/>
+            <Header />
+            <WorkTime />
             <main className="main">
-                <Projects/>
-                <GraphTon/>
-                <Calculator/>
-                <Contacts/>
+                <Projects />
+                <GraphTon />
+                <Calculator />
+                <GoldView />
+                <Contacts />
             </main>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
